e4e4ec937fadcecd9664baa44d87605d38acfc10
Guillaume Houioux <guillaume.houioux@smals.be>
2024-07-26 11:07:15 +0200

    confetti

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {ClassicalBoardComponent} from "../board/classical-board/classical-board.component";
 import {DatePipe, NgIf} from "@angular/common";
 import {TimerComponent} from "../timer/timer.component";
+import { confetti } from 'tsparticles-confetti';
 
 @Component({
   selector: 'game',
@@ -29,14 +30,47 @@ export class GameComponent {
     this.timer.clearTimer();
     this.timer.startTimer();
   }
+  
   updateGameStatus(status: string) {
-    if(status == 'ONGOING' || status == 'WON' || status == 'GAMEOVER') {
-      if (status == 'WON' || status == 'GAMEOVER'){
-        this.playingTime = this.timer.counter;
-        this.timer.clearTimer();
+    if (status === 'WON' || status === 'GAMEOVER') {
+      if (status === 'WON') {
+        this.triggerConfettis();
       }
-      if(status == 'ONGOING') this.timer.startTimer();
+      this.playingTime = this.timer.counter;
+      this.timer.clearTimer();
+      this.result = status;
+    } else if (status === 'ONGOING') {
+      this.timer.startTimer();
       this.result = status;
     }
+  }
+
+  triggerConfettis() {
+    const duration = 15 * 1000,
+    animationEnd = Date.now() + duration,
+    defaults = { startVelocity: 20, spread: 360, ticks: 60, zIndex: 0 };
+
+    const interval = setInterval(function() {
+      const timeLeft = animationEnd - Date.now();
+    
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+    
+      const particleCount = 30 * (timeLeft / duration);
+    
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: Math.random() * (0.3 - 0.1) + 0.1, y: Math.random() - 0.2 },
+        })
+      );
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: Math.random() * (0.9 - 0.7) + 0.7, y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
   }
 }

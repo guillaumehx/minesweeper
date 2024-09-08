6f3a7e2bd0ba31bfcbc3ad86563164d672184456
gloubi-boulga <guillaumehx@outlook.com>
2024-09-08 14:33:30 +0200

    add transloco and language management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { GameComponent } from "./game/game.component";
+import { Component, OnInit } from '@angular/core';
+import { GameComponent } from './game/game.component';
+import { TranslocoService } from '@jsverse/transloco';
+import { StorageService } from './service/storage/storage.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameComponent],
-  templateUrl: './app.component.html'
+  imports: [GameComponent],
+  templateUrl: './app.component.html',
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(
+    private storageService: StorageService,
+    private translocoService: TranslocoService,
+  ) {}
+
+  ngOnInit(): void {
+    this.translocoService.setActiveLang(this.storageService.getLanguage());
+  }
+
+  switchLanguage(language: string) {
+    this.storageService.setLanguage(language);
+    this.translocoService.setActiveLang(language);
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'fr'],
+        defaultLang: 'en',
+        fallbackLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };

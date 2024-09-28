755adc8aa4910b906ce118238aaea693aebe0ace
gloubi-boulga <guillaumehx@outlook.com>
2024-09-28 15:07:43 +0200

    new version

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -7,6 +7,8 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(
+      `https://gloubi-boulga.github.io/minesweeper/assets/i18n/${lang}.json`,
+    );
   }
 }
--- transloco.config.ts
+++ transloco.config.ts
@@ -1,7 +1,7 @@
 import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
 
 const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'src/assets/i18n/',
+  rootTranslationsPath: 'https://gloubi-boulga.github.io/minesweeper/assets/i18n/',
   langs: [ 'en', 'fr' ],
   keysManager: {}
 };

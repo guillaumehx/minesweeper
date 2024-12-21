40f5dba176e0156619a14c6f65947fc9c0838d6d
gloubi-boulga <guillaumehx@outlook.com>
2024-12-21 13:09:42 +0100

    it speaks for itself
    Co-authored-by: geektortoise <renaud.hoyoux@yahoo.fr>

--- src/app/tile/tile.ts
+++ src/app/tile/tile.ts
@@ -1,13 +1,8 @@
 export class Tile {
-  // Id of the tile
   id: number;
-  // Whether or not the tile is revealed
   isRevealed: boolean;
-  // Whether or not the tile is a mine
   isMine: boolean;
-  // Whether or not the tile has been flagged as a mine by the player
   isFlagged: boolean;
-  // the neighboorhood
   neighbors: Tile[];
 
   constructor(id: number, isMine: boolean) {
